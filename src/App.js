88a86fb758a72fae8cc3d6f859f067789b0742af
Shriprada M N <90555524+shripradamn@users.noreply.github.com>
2024-01-08 20:01:26 +0530

    react practice

---
+++ src/App.js
@@ -0,0 +1,44 @@
+// App.js
+import React from 'react';
+function Nav(){
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Navbar
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a>Home</a>
+              </li>
+              <li className="nav-item">
+                <a>About</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+  )
+}
+
+const App = () => {
+  return (
+    <>
+    <Nav/>
+    </>
+  );
+};
+
+export default App;
